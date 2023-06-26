070a47471c2f759a7252a55d2a2ba33dd1b8630c
JolyDone <twilightclimb@gmail.com>
2023-06-26 21:28:33 +0300

    /

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Util {
-    public static void main(String args[]) {
+    public void getConnection() {
         Connection c = null;
         try {
             Class.forName("org.postgresql.Driver");
